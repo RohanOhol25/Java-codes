89941112f6d8259e0f48f25c70b25a6260a3f495
Rohan <166725069+RohanOhol25@users.noreply.github.com>
2025-08-01 18:49:42 +0530

    Create a Function In Java.
    Create a Function In Java.

---
+++ function.java
@@ -0,0 +1,10 @@
+public class function {
+    public static void greet(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        greet("Rohan");
+        greet("Alice");
+    }
+}
